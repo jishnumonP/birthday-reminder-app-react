e1a8368d6a8c0569dd0707ea49a8ed5eae6b263c
jishnumonP <jishnup981@gmail.com>
2023-06-14 20:52:06 +0530

    Second

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { useState } from 'react'
 import {data} from './StaticData/data'
 import List from './components/List/List'
+import Home from './components/Home/Home'
 function App() {
   const [people, setPeople] = useState(data)
   return (
-    <main>
-      <section className='container'>
-        <h3>{people.length} birthdays today</h3>
-        <List people={people} />
-        <button onClick={() => setPeople([])}>clear all</button>
-      </section>
-    </main>
+
+<>
+<Home/>
+  </>
   )
 }
 
