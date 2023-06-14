469da1f95247f18ea251c7767199c9e5517c397d
jishnumonP <jishnup981@gmail.com>
2023-06-14 21:03:49 +0530

    Second

--- src/StaticData/data.js
+++ src/StaticData/data.js
@@ -1,37 +1,44 @@
 export const  data = [
   {
     id: 1,
-    name: 'Bertie Yates',
+    name: 'Aiswarya',
     age: 29,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
   },
   {
     id: 2,
-    name: 'Hester Hogan',
+    name: 'Amidha',
     age: 32,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
   },
   {
     id: 3,
-    name: 'Larry Little',
+    name: 'Hari Shanker',
     age: 36,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
   },
   {
     id: 4,
-    name: 'Sean Walsh',
+    name: 'Mithun Raj',
     age: 34,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
   },
   {
     id: 5,
-    name: 'Lola Gardner',
+    name: 'Athul Mohan',
     age: 29,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      'https://images.generated.photos/43N1jLqgRA0Q7_gXIEEVtNsNjrkM0TWdPNOGDjjrpKI/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/ODA5MjMxLmpwZw.jpg',
+  },
+  {
+    id: 5,
+    name: 'Rahul',
+    age: 19,
+    image:
+      'https://images.generated.photos/XdKwfJpngPuyiPsKg5eVb1RbiweNDUAlz5kHJ4Z2yRU/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTEwMDY4LmpwZw.jpg',
   },
 ];
